a26efc12bc32b25f36a32775af52c43aa9a6a710
Ivan Shumkov <ivan@shumkov.ru>
2020-06-09 20:41:43 +0300

    Update dpns.spec.js

--- test/e2e/dpns.spec.js
+++ test/e2e/dpns.spec.js
@@ -1,5 +1,19 @@
-describe('e2e', () => {
-  describe('DPNS', () => {
+describe('DPNS', () => {
+  // Use https://github.com/mochajs/mocha/issues/2894#issuecomment-492979837
 
+  it('should exists');
+
+  describe('DPNS owner', () => {
+    // generate a random one which will be used in tests above
+    // skip if DPNS owner private key is not passed and use `dash` in tests above
+    it('should be able to register a TLD');
+  });
+
+  describe('Any Identity', () => {
+    it('should not be able to register TLD');
+    it('should be able to register a second level domain');
+    it('should not be able to register a subdomain for parent domain which is not exist');
+    it('should be able to search a domain');
+    it('should be able to resolve domain by it\'s record');
   });
 });
