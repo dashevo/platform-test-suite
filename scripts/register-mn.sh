88404e290aa82940b164722c334f1ccda9515e08
Konstantin Shuplenkov <konstantin.shuplenkov@dash.org>
2020-06-17 14:10:38 +0300

    Debug

--- scripts/register-mn.sh
+++ scripts/register-mn.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+mn wallet:generate-to-address local 2000
 export PRIVATE_KEY=$(mn wallet:generate-to-address local 2000 | grep -m 1 "Private key:" | awk '{printf $3}')
 export OPERATOR_BLS_KEY=$(mn register local $PRIVATE_KEY 127.0.0.1 20001 | grep -m 1 "Private key:" | awk '{printf $3}')
 
 echo "##############################"
 echo  env
 echo "##############################"
-echo  PRIVATE_KEY $PRIVATE_KEY
+echo  PRIVATE_KEY ${PRIVATE_KEY}
 echo  OPERATOR_BLS_KEY $OPERATOR_BLS_KEY
