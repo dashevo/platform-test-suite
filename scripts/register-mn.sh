aff53c0b5eb87a33f3e8855087f9d3c75ed8885d
Konstantin Shuplenkov <konstantin.shuplenkov@dash.org>
2020-06-17 17:04:01 +0300

    Debug

--- scripts/register-mn.sh
+++ scripts/register-mn.sh
@@ -2,6 +2,7 @@
 
 mn wallet:generate-to-address local 2000
 docker ps -a
+docker ps -a -q | xargs -L 1 docker logs
 export PRIVATE_KEY=$(mn wallet:generate-to-address local 2000 | grep -m 1 "Private key:" | awk '{printf $3}')
 export OPERATOR_BLS_KEY=$(mn register local $PRIVATE_KEY 127.0.0.1 20001 | grep -m 1 "Private key:" | awk '{printf $3}')
 
