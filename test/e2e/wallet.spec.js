677cb4fb58479b920cbca56e464d15fea2a4031b
Djavid Gabibiyan <djavidoff@gmail.com>
2020-08-12 21:11:43 +0500

    chore: initial version of `wallet` e2e test

---
+++ test/e2e/wallet.spec.js
@@ -0,0 +1,108 @@
+const Dash = require('dash');
+
+const getDAPISeeds = require('../../lib/test/getDAPISeeds');
+
+const createClientWithFundedWallet = require('../../lib/test/createClientWithFundedWallet');
+const createClientWithoutWallet = require('../../lib/test/createClientWithoutWallet');
+
+describe('e2e', () => {
+  describe('Wallet', function main() {
+    this.timeout(950000);
+
+    let fundedWallet;
+    let emptyWallet;
+    let duplicateWallet;
+
+    before(async () => {
+      fundedWallet = await createClientWithFundedWallet();
+      emptyWallet = await createClientWithoutWallet();
+    });
+
+    after(async () => {
+      if (fundedWallet) {
+        await fundedWallet.disconnect();
+      }
+
+      if (emptyWallet) {
+        await emptyWallet.disconnect();
+      }
+
+      if (duplicateWallet) {
+        await duplicateWallet.disconnect();
+      }
+    });
+
+    describe('empty wallet', () => {
+      it('should have no transaction at first', async () => {
+        const emptyAccount = await emptyWallet.getAccount();
+
+        expect(emptyAccount.getTransactions()).to.be.empty();
+      });
+
+      it('should receive a transaction when as it has been sent', async () => {
+        const emptyAccount = await emptyWallet.getAccount();
+        const fundedAccount = await fundedWallet.getAccount();
+
+        const tx = await fundedAccount.createTransaction({
+          recipient: {
+            amount: 10,
+            address: emptyAccount.getUnusedAddress(),
+          },
+        });
+
+        await fundedAccount.broadcastTransaction(tx);
+
+        expect(emptyAccount.getTransactions()).to.have.lengthOf(1);
+
+        // TODO: check tx is exactly the same as the sent one
+      });
+    });
+
+    describe('duplicate wallet', () => {
+      it('should have all transaction from before at first', async () => {
+        const mnemonic = 'TODO: get mnemonic';
+
+        duplicateWallet = new Dash.Client({
+          wallet: {
+            mnemonic,
+          },
+          seeds: getDAPISeeds(),
+        });
+
+        const duplicateAccount = await duplicateWallet.getAccount();
+
+        expect(duplicateAccount.getTransactions()).to.have.lengthOf(1);
+
+        // TODO: check tx is exactly the same as the sent one
+      });
+
+      it('should receive a transaction when as it has been sent', async () => {
+        const duplicateAccount = await duplicateWallet.getAccount();
+        const fundedAccount = await fundedWallet.getAccount();
+
+        const tx = await fundedAccount.createTransaction({
+          recipient: {
+            amount: 10,
+            address: duplicateAccount.getUnusedAddress(),
+          },
+        });
+
+        await fundedAccount.broadcastTransaction(tx);
+
+        expect(duplicateAccount.getTransactions()).to.have.lengthOf(1);
+
+        // TODO: check tx is exactly the same as the sent one
+      });
+    });
+
+    describe('empty wallet', () => {
+      it('should receive a transaction when as it has been sent to duplicate wallet', async () => {
+        const emptyAccount = await emptyWallet.getAccount();
+
+        expect(emptyAccount.getTransactions()).to.have.lengthOf(2);
+
+        // TODO: check txs are exactly the same as the sent ones
+      });
+    });
+  });
+});
