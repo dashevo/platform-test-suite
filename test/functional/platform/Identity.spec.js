be16f15d763727317b19cab04f1107757688507b
Konstantin Shuplenkov <konstantin.shuplenkov@dash.org>
2021-03-10 16:35:50 +0300

    debug

--- test/functional/platform/Identity.spec.js
+++ test/functional/platform/Identity.spec.js
@@ -173,8 +173,8 @@ describe('Platform', () => {
 
       expect(broadcastError).to.exist();
 
-      expect(broadcastError.code).to.be.equal(2);
       expect(broadcastError.message).to.be.equal('Invalid state transition: IdentityPublicKeyAlreadyExistsError: Identity public key already exists');
+      expect(broadcastError.code).to.be.equal(2);
       const [error] = broadcastError.data.errors;
       expect(error.name).to.equal('IdentityPublicKeyAlreadyExistsError');
       expect(Buffer.from(error.publicKeyHash)).to.deep.equal(identity.getPublicKeyById(0).hash());
