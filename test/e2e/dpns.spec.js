88898d423d5664a061651f94ab07a0df9565c3bd
Anton Suprunchuk <antouhou@gmail.com>
2021-04-07 18:51:34 +0300

    revert last change

--- test/e2e/dpns.spec.js
+++ test/e2e/dpns.spec.js
@@ -5,7 +5,6 @@ const createClientWithFundedWallet = require('../../lib/test/createClientWithFun
 const getRandomDomain = () => crypto.randomBytes(10).toString('hex');
 
 describe('DPNS', () => {
-
   let failed = false;
   let client;
   let identity;
